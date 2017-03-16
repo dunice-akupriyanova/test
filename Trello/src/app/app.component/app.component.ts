943b27b10f51ef16e2d37cbb91b152d3e3434912
User Name <root@localhost>
2017-03-16 23:00:02 +0300

    Some little change

--- Trello/src/app/app.component/app.component.ts
+++ Trello/src/app/app.component/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {Card} from '../classes/classes';
 import {List} from '../classes/classes';
 import {Board} from '../classes/classes';
+import {ListComponent} from '../list.component/list.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,24 +10,28 @@ import {Board} from '../classes/classes';
 })
 export class AppComponent {
   now=new Date();
-  card1: Card=new Card(0, 'Card1', 'Description1', this.now.toLocaleString());
-  card2: Card=new Card(1, 'Card2 Card2 Card2 Card2 Card2 Card2 Card2 Card2', 'Description2', this.now.toLocaleString());
-  card3: Card=new Card(2, 'Card3', 'Description3', this.now.toLocaleString());
-  card4: Card=new Card(3, 'Card4', 'Description4', this.now.toLocaleString());
-  card5: Card=new Card(0, 'card5', 'Description5', this.now.toLocaleString());
-  card6: Card=new Card(1, 'Card6', 'Description6', this.now.toLocaleString());
-  card7: Card=new Card(2, 'Card7', 'Description7', this.now.toLocaleString());
-  card8: Card=new Card(3, 'Card8', 'Description8', this.now.toLocaleString());
+  card1: Card=new Card('Card1', 'Description1', this.now.toLocaleString());
+  card2: Card=new Card('Card2 Card2 Card2 Card2 Card2 Card2 Card2 Card2', 'Description2', this.now.toLocaleString());
+  card3: Card=new Card('Card3', 'Description3', this.now.toLocaleString());
+  card4: Card=new Card('Card4', 'Description4', this.now.toLocaleString());
+  card5: Card=new Card('card5', 'Description5', this.now.toLocaleString());
+  card6: Card=new Card('Card6', 'Description6', this.now.toLocaleString());
+  card7: Card=new Card('Card7', 'Description7', this.now.toLocaleString());
+  card8: Card=new Card('Card8', 'Description8', this.now.toLocaleString());
   list1: List=new List('list1', [this.card1, this.card2]);
   list2: List=new List('list2', [this.card3, this.card4]);
   list3: List=new List('list3', [this.card5, this.card6]);
   list4: List=new List('list4', [this.card7, this.card8]);
-  board1: Board= new Board(3, 'Board1 Board1 Board1', [this.list1, this.list2]);
-  board2: Board= new Board(3, 'Board2', [this.list3, this.list4]);
+  board1: Board= new Board('Board1 Board1 Board1', [this.list1, this.list2]);
+  board2: Board= new Board('Board2', [this.list3, this.list4]);
   boards: Array<Board>=[this.board1, this.board2];
   currentBoard: Board=this.boards[0];
   newName: string;
+  currentCard: Card=new Card('', '', '');  
   newBoardName: string;
+  showCurrent(): void {
+	  this.currentCard=ListComponent.currentCard;
+  }
   addList(newName): void {
     if (!newName) {return}
     this.currentBoard.lists.push(new List(newName, []));
@@ -34,7 +39,7 @@ export class AppComponent {
   }
   addBoard(newName): void {
     if (!newName) {return}
-    this.boards.push(new Board(0, newName, []));
+    this.boards.push(new Board(newName, []));
     this.newBoardName='';
     if (this.boards.length==1) {
       this.currentBoard=this.boards[0];
--- Trello/src/app/list.component/list.component.ts
+++ Trello/src/app/list.component/list.component.ts
@@ -34,61 +34,17 @@ export class ListComponent {
   removeList(list): void {
     this.pull(this.currentBoard.lists, list);
   }
+  static currentCard: Card=new Card('', '', '');
   newCardName: string;
   now=new Date();
   addCard(newCardName, board): void {
     this.now=new Date();
     if (!newCardName) {return}
-    board.count++;
-    this.list.cards.push(new Card(board.count, newCardName, '', this.now.toLocaleString()));
+    this.list.cards.push(new Card(newCardName, '', this.now.toLocaleString()));
     this.newCardName='';
   }
   showDetails(card, list): void {
     document.getElementById('window_overlay').classList.add('show');
-    var detailsContent=document.getElementById('window_content');
-    var containers=document.getElementsByClassName(card.id);
-    var container=containers[0];
-    var card_names=document.getElementsByClassName(card.name);
-    var insert_name=card_names[0];
-    if (insert_name){
-      var insert_name_clone=insert_name.cloneNode(true);
-    }
-    var card_descriptions=document.getElementsByClassName(card.description);
-    var insert_descriptions=card_descriptions[0];
-    if (insert_descriptions) {
-      var insert_descriptions_clone=insert_descriptions.cloneNode(true);
-    }
-    if (insert_name_clone){
-      container.appendChild(insert_name_clone);
-    }
-    if (insert_descriptions_clone) {
-      container.appendChild(insert_descriptions_clone);
-    }
-    console.log('before');
-    for (let i=0; i<container.childNodes.length; i++) {
-      console.log(container.childNodes[i]);
-    }
-    // document.body.appendChild(insert_name);
-    // document.body.appendChild(insert_descriptions);
-    detailsContent.innerHTML='';
-    if (insert_name) {
-      detailsContent.appendChild(insert_name);
-    }
-    if (insert_descriptions) {
-      detailsContent.appendChild(insert_descriptions);
-    }
-    if (insert_name_clone){
-      container.appendChild(insert_name_clone);
-    }
-    if (insert_descriptions_clone) {
-      container.appendChild(insert_descriptions_clone);
-    }
-    console.log('after');
-    console.log("clone");
-    // console.log(insert_name_clone);
-    // console.log(insert_descriptions_clone);
-    for (let i=0; i<container.childNodes.length; i++) {
-      console.log(container.childNodes[i]);
-    }
+	ListComponent.currentCard=card;
   }
 }
